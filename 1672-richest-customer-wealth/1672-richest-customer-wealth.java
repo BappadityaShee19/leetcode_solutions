70ef49061f7384ad6080a11ebcd98fde0f1f12a7
Bappaditya's Lab <yashkaramevents@gmail.com>
2026-08-30 09:11:59 +0530

    Time: 3 ms (1.3%), Space: 45.1 MB (6.02%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int[] sum = new int[accounts.length];
+        for(int i=0; i<accounts.length; i++){
+            for(int j=0; j<accounts[i].length; j++){
+                sum[i]+= accounts[i][j];
+            }
+        }
+        Arrays.sort(sum);
+        return sum[accounts.length-1];
+    }
+}
